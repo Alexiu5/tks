eb17243a3aa81cfdce34696ee24da21ffc05d9a0
Alex <alexis.higuita@skyrocketlab.space>
2021-11-20 16:07:41 -0500

    [TEST] app.js h1 changed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './styles/output.css'
 function App() {
     return (
         <div className="bg-gray-900 p-20 h-screen flex justify-center items-start flex-col">
-            <h1 className="text-5xl text-white">The keeb store is under construction 🚀</h1>
+            <h1 className="text-5xl text-white">We're working on it 🚀</h1>
             <p className="text-gray-400 mt-5 text-lg">
                 Please comeback later
             </p>
